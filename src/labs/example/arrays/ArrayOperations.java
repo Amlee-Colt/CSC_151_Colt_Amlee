08cfe43e400075630a9a9f6fd1490045db7b4b78
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2026-03-18 11:52:26 -0400

    18 March 2026

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -1,7 +1,7 @@
 /*
 @author: Colt Amlee
-@date: 2 March 2026
-@purpose: Week 8 lab
+@date: 18 March 2026
+@purpose: Week 10 lab
 */
 
 package labs.example.arrays;
@@ -44,5 +44,73 @@ public class ArrayOperations {
         for (int i = 0; i < dayArray.length; i++) {
             System.out.println("There are " + dayArray[i] + " days in " + monthArray[i]);
         }
+    }
+    
+    public static void compareArray(int firstArray[], int secondArray[]) {
+        int firstArrayLength = 0;
+        int secondArrayLength = 0;
+        boolean equal = true;
+        for (firstArrayLength = 0; firstArrayLength < firstArray.length; firstArrayLength++) {}
+        for (secondArrayLength = 0; secondArrayLength < secondArray.length; secondArrayLength++) {}
+        if (firstArrayLength != secondArrayLength){
+            System.out.println("These arrays are not the same length.");
+
+            for (int i = 0; i < Math.min(firstArrayLength, secondArrayLength); i++) {
+                if (firstArray[i] != secondArray[i]) {
+                    System.out.println("These arrays do not contain the same values.");
+                    equal = false; 
+                    break;                    
+                }
+            }
+            if (equal == true) {
+                System.out.println("The larger array contains all " + Math.min(firstArrayLength, secondArrayLength) + " elements of the smaller array.");}
+        } 
+        else {
+            System.out.println("These arrays are the same length.");
+            for (int i = 0; i < firstArrayLength; i++) {
+
+                if (firstArray[i] != secondArray[i]) {
+                    System.out.println("These arrays do not contain the same values.");
+                    equal = false; 
+                    break;
+                }
+            }
+            if (equal == true) {
+                System.out.println("These two arrays have the same values.");}
+        }
+
+    }
+
+    public static void sumArray(int arr1[], int arr2[], int arr3[]) {
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+//        for (int i = 0; i < arr1.length; i++) {
+//            sum1 += arr1[i];
+//        }
+//        System.out.println(sum1);
+
+        for (int i : arr1) {
+        sum1 += i;
+        }
+        System.out.println("The value of arr1 is " + sum1);
+        for (int i : arr2) {
+        sum2 += i;
+        }
+        System.out.println("The value of arr2 is " + sum2);
+        for (int i : arr3) {
+        sum3 += i;
+        }
+        System.out.println("The value of arr3 is " + sum3);
+        if (sum1 > sum2 && sum1 > sum3) {
+            System.out.println("The array with the highest sum is arr1 with a value of " + sum1 + ".");
+        } else if (sum2 > sum1 && sum2 > sum3) {
+            System.out.println("The array with the highest sum is arr2 with a value of " + sum2 + ".");
+        } else if (sum3 > sum1 && sum3 > sum2) {
+            System.out.println("The array with the highest sum is arr3 with a value of " + sum3 + ".");
+        } else {
+            System.out.println("One of the arrays is equal to another and I'm too lazy to account for that as of now. \nThat isn't a scenario possible with the current values of my arrays and I don't imagine you'd go into that much depth whilst grading, but I'm still leaving this here since it's technically possible.");
+        }
+
     }
 }
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -8,10 +8,24 @@ package labs.example.arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        ArrayOperations.createNewArray(15);
-        System.out.println("-------------------------");
-        ArrayOperations.sortArray(new int[]{5, 3, 8, 1, 2, 22, 9, 0, 15, 12});
-        System.out.println("-------------------------");
-        ArrayOperations.getDaysAndMonths();
+        //ArrayOperations.createNewArray(15);
+        //System.out.println("-------------------------");
+        //ArrayOperations.sortArray(new int[]{5, 3, 8, 1, 2, 22, 9, 0, 15, 12});
+        //System.out.println("-------------------------");
+        //ArrayOperations.getDaysAndMonths();
+
+        //  int firstArray[] = {10,25,2,7,14,1,1,5,9,8,6};
+        //int secondArray[] = {10,4,17,8,5,1,5,3,7,9};
+        //  int secondArray[] = {10,25,2,7,14,1,1,5,9,8,6};
+        //int secondArray[] = {10,25,2,7,14,1,1,5,9,8};
+        //int secondArray[] = {10,25,2,7,14,1,1,5,9,8,5};
+        //  ArrayOperations.compareArray(firstArray, secondArray);
+
+        int arr1[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}; //1-25
+        int arr2[] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368}; //fibonacci sequence
+        int arr3[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; //1*25
+        ArrayOperations.sumArray(arr1, arr2, arr3);
+
+
     }
 }
