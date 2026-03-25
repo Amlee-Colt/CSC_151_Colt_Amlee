caca4b1fb4d9e1a1afe190bf7f51b66e1476af6c
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2026-03-25 11:17:42 -0400

    25 March 2026

--- src/labs/example/fileOperations/fileOperations.java
+++ src/labs/example/fileOperations/fileOperations.java
@@ -4,23 +4,25 @@
 @purpose: Week 11 lab
 */
 
+package labs.example.fileOperations;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 
-public class fileOperations {
+public class FileOperations { //reads CSV
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("T:\\CSC_151_Colt_Amlee\\src\\labs\\example\\fileOperations\\\\users.csv"))) {
             String line;
-            //double mathTotal = 0;
             while ((line = br.readLine()) != null) {
                 String values[] = line.split(","); // Splits by comma
-                // Process values[]
                 System.out.printf("%-7s|%-4s|%-7s|%-7s|%n", values[0], values[1], values[2], values[3]);
+                
             }
         } catch (IOException e) { e.printStackTrace(); }
 
 
     }
+
 }
