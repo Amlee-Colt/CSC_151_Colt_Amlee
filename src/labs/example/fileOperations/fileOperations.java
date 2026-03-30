b301a79d4d597b58d84d8bdbfdce54acbcfc6946
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2026-03-30 11:21:38 -0400

    30 March 2026

--- src/labs/example/fileOperations/fileOperations.java
+++ src/labs/example/fileOperations/fileOperations.java
@@ -12,17 +12,25 @@ import java.io.IOException;
 
 
 public class FileOperations { 
+
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("T:\\CSC_151_Colt_Amlee\\src\\labs\\example\\fileOperations\\\\users.csv"))) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("T:\\CSC_151_Colt_Amlee\\src\\labs\\example\\fileOperations\\users.csv"))) {
             String line;
+            int count = 0;
             while ((line = br.readLine()) != null) {
                 String values[] = line.split(","); // Splits by comma
-                System.out.printf("%-7s|%-4s|%-7s|%-7s|%n", values[0], values[1], values[2], values[3]);
+                if (count > 0){
+                    System.out.printf("%-7s|%-4s|%-7s|%-7s|%-7s|%n", values[0], values[1], values[2], values[3], (Integer.parseInt(values[1]) + Integer.parseInt(values[2]) + Integer.parseInt(values[3]))/3);}
+                    else {
+                        System.out.printf("%-7s|%-4s|%-7s|%-7s|Average|%n-------------------------------------%n", values[0], values[1], values[2], values[3]);
+                        count++;
+                    }
                 
             }
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException e) { e.printStackTrace(); System.out.println(e);}
 
-
+        
     }
 
 }
