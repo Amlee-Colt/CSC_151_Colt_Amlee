d12c1502e4785c9c8652cab5d87d3221c32e69c1
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2026-04-01 11:40:43 -0400

    1 April 2026

--- src/labs/example/fileOperations/fileOperations.java
+++ src/labs/example/fileOperations/fileOperations.java
@@ -1,6 +1,6 @@
 /*
 @author: Colt Amlee
-@date: 23 March 2026
+@date: 1 April 2026
 @purpose: Week 11 lab
 */
 
@@ -14,6 +14,11 @@ import java.io.IOException;
 public class FileOperations { 
 
     public static void main(String[] args) {
+        csvReader();
+        //logReader();
+    }
+
+    public static void csvReader(){
 
         try (BufferedReader br = new BufferedReader(new FileReader("T:\\CSC_151_Colt_Amlee\\src\\labs\\example\\fileOperations\\users.csv"))) {
             String line;
@@ -28,9 +33,36 @@ public class FileOperations {
                     }
                 
             }
-        } catch (IOException e) { e.printStackTrace(); System.out.println(e);}
-
-        
+        } catch (IOException e) { e.printStackTrace();}
     }
 
-}
+    public static void logReader(){
+        int errorTally = 0;
+        int warningTally = 0;
+        int infoTally = 0;
+        int totalTally = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("T:\\CSC_151_Colt_Amlee\\src\\labs\\example\\fileOperations\\api_error.log"))) {
+            String line;
+            
+            while ((line = br.readLine()) != null) {
+
+                //System.out.println(line); //This just prints out every line
+                String severityLevel = line.substring(line.indexOf("[") + 1,line.indexOf("]"));
+                if (severityLevel.equals("ERROR")) {
+                    errorTally += 1;
+                } else if (severityLevel.equals("WARN")) {
+                    warningTally += 1;
+                } else if (severityLevel.equals("INFO")){
+                    infoTally += 1;
+                }
+                totalTally += 1;
+                
+                //Need to figure out what logic to use to detect when a "flag?" character is used and a way to turn data into usable formats. ie, a way to detect the brackets and store whether something is an error, a warning, or just info.
+                //From there, a way to tally how much of each, and what types of [errors/warnings/info] are logged. For instance, return how many errors there are, and a tally of each type of error.
+                //I could theoretically just go through each line character by character and print every index starting after a certain char like "[" until we get to "]", but how would I convert this into a usable format? What flexibility do I have when I switch between different separators?
+            }
+            System.out.printf("Amount of errors: %s%nAmount of warnings: %s%nAmount of info: %s%nTotal logs: %s", errorTally, warningTally, infoTally, totalTally);
+        }
+        catch (IOException e) { e.printStackTrace();}}
+    }
---
+++ src/labs/example/fileOperations/test.java
@@ -0,0 +1,11 @@
+package labs.example.fileOperations;
+
+public class test {
+    public static void main(String[] args) {
+        String severityLevel = "ERROR";
+        if (severityLevel == "ERROR") {
+            System.out.println("Success");
+        }
+        }
+
+}
